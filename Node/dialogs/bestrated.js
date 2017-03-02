a14c317cd6930a04a6062af84eda796a49933b95
Thiago Almeida <thalme@microsoft.com>
2017-03-02 15:09:13 -0800

    Moving some logic

--- Node/dialogs/bestrated.js
+++ Node/dialogs/bestrated.js
@@ -1,5 +1,5 @@
 var builder = require('botbuilder');
-var yelpsearch = require('../util/yelpsearch');
+var yelputil = require('../util/yelp');
 
 module.exports = [
     function (session) {
@@ -9,35 +9,19 @@ module.exports = [
         if (results)
         {
             session.send("Thanks, got it! Here's 10 recommendations from Yelp close to you that are open now:");
-            yelpsearch.getYelpRecommendations(results.response.geo.latitude, results.response.geo.longitude)
+            yelputil.getYelpRecommendations(results.response.geo.latitude, results.response.geo.longitude)
             .then(places => {
-                var cards = [];
-                places.forEach(place => {
-                    var display_address = place.location.display_address.join(','); //combine address lines
-                    var maps_url = 'http://maps.google.com/?daddr=' + display_address.split(' ').join('+');
-                    cards.push(
-                        new builder.HeroCard(session)
-                        .title(place.name)
-                        .subtitle(display_address + '\r\n' + place.display_phone)
-                        .text('Rating: ' + place.rating + '. Reviews: ' + place.review_count)
-                        .images([
-                            builder.CardImage.create(session, place.image_url)
-                        ])
-                        .buttons([
-                            builder.CardAction.openUrl(session, place.url, 'Learn More'),
-                            builder.CardAction.openUrl(session, maps_url, 'Directions')
-                        ])
-                    )
-                });
+                var cards = yelputil.getCardsFromPlaces(session, places);
                 var reply = new builder.Message(session)
                     .attachmentLayout(builder.AttachmentLayout.carousel)
                     .attachments(cards);
                 session.send(reply);
+                session.replaceDialog('/');
             })
         }
         else
         {
-            session.send('Couldn\'t find your location... Better luck next time!');
+            session.send('Couldn\'t get your location... Better luck next time!');
             session.replaceDialog('/');
         }
     }
---
+++ Node/util/yelp.js
@@ -0,0 +1,46 @@
+var builder = require('botbuilder');
+var yelp = require('yelp-fusion');
+const clientId = process.env.YELP_APP_ID;
+const clientSecret = process.env.YELP_APP_TOKEN;
+
+module.exports = {
+    getYelpRecommendations: function(lat, long) {
+        return yelp.accessToken(clientId, clientSecret).then(response => {
+            var client = yelp.client(response.jsonBody.access_token);
+            var searchRequest = {
+                latitude: lat,
+                longitude: long,
+                radius: 8046, //5 miles
+                sort_by: "rating",
+                open_now: true,
+                limit:10,
+                categories:'food'
+            };
+            return client.search(searchRequest).then(response => {
+                var businesses = response.jsonBody.businesses;
+                return businesses;
+            });
+        });
+    },
+    getCardsFromPlaces: function(session, places) {
+        var cards = [];
+        places.forEach(place => {
+            var display_address = place.location.display_address.join(','); //combine address lines
+            var maps_url = 'http://maps.google.com/?daddr=' + display_address.split(' ').join('+');
+            cards.push(
+                new builder.HeroCard(session)
+                .title(place.name)
+                .subtitle(display_address + '\r\n' + place.display_phone)
+                .text('Rating: ' + place.rating + '. Reviews: ' + place.review_count)
+                .images([
+                    builder.CardImage.create(session, place.image_url)
+                ])
+                .buttons([
+                    builder.CardAction.openUrl(session, place.url, 'Learn More'),
+                    builder.CardAction.openUrl(session, maps_url, 'Directions')
+                ])
+            )
+        });
+        return cards;
+    }
+}
